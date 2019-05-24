adae8b9f78529d821d293292bb6ca2e8676fd471
Martinlinux1 <matko.micuch@gmail.com>
2019-05-24 17:59:44 +0200

    bla

---
+++ serverUtils/fileloader.go
@@ -0,0 +1,22 @@
+package serverUtils
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+type FileLoader struct {
+	Path string
+}
+
+func (fileLoader FileLoader) LoadFile(writer http.ResponseWriter, request *http.Request) {
+	form, err := ioutil.ReadFile(fileLoader.Path)
+
+	if err != nil {
+		return
+	}
+
+	if _, err := writer.Write(form); err != nil {
+		return
+	}
+}
